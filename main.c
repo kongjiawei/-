d8c7d02bc62ae5c5059799a741ff6c423ac733fd
iflytang <iflytang@qq.com>
2019-10-13 03:02:44 -0400

    add some counters

--- main.c
+++ main.c
@@ -113,11 +113,16 @@
 /* define 50ms. */
 #define FIFTY_MS         50000.0
 
-#define ERROR_THRESH 0.2   /* 20% */
+#define ERROR_THRESH 0.01   /*  in percentage */
+
+#define PRINT_INT_FIELDS            true
+#define PRINT_SECOND_PERFORMANCE    true
+#define PRINT_SW_CNT                true
 
 /* map_info + switch_id +in_port + out_port + hop_latency + ingress_time + bandwidth + relative_time + cnt. */
 static char * INT_FMT = "%x\t%u\t%u\t%u\t%u\t%llx\t%f\t%f\t%u\n";
-static char * INT_STR = "map_info\tsw\tin_port\tout_port\tdelay\ttime\tbandwidth\trelative_time\tcnt\n";
+static char * INT_FIELDS_STR = "map_info\tsw\tin_port\tout_port\tdelay\tin_time\tbandwidth\trelative_time\tcnt%s\n";
+static char * INT_PKT_CNT_STR = "sec\t recv_cnt\t sw0\t sw1\t sw2\t sw3\t sw4\t sw5\t sw6%s\n";
 
 /* INT Header: Metadata set. */
 typedef struct {
@@ -146,7 +151,7 @@ FILE *fp;
 static void print_pkt(uint32_t pkt_len, uint8_t *pkt){
     uint32_t i = 0;
     for (i = 0; i < pkt_len; ++i) {
-        printf("%02x", i, pkt[i]);
+        //printf("%02x", i, pkt[i]);
 
         if ((i + 1) % 8 == 0) {
             printf(" ");   // 2 space
@@ -165,28 +170,29 @@ static inline unsigned long long rp_get_us(void) {
 }
 
 /* used as 'hash' condition for statistics. 'switch_id' or 'ttl' as index. */
-uint32_t his_hash[MAX_DEVICE] = {0}, hash[MAX_DEVICE] = {1, 1, 1, 1, 1, 1};
+uint32_t his_hash[MAX_DEVICE+1] = {0}, hash[MAX_DEVICE+1] = {1, 1, 1, 1, 1, 1};
 
 /* used as 'time_flag' condition for statistics. 'switch_id' or 'ttl' as index. */
-uint16_t last_hop_latency[MAX_DEVICE] = {1, 1, 1, 1, 1, 1}, time_flag[MAX_DEVICE] = {0};
+uint16_t last_hop_latency[MAX_DEVICE+1] = {1, 1, 1, 1, 1, 1}, time_flag[MAX_DEVICE+1] = {0};
 
 /* used as 'cnt_threshold' condition for statistics. 'switch_id' or 'ttl' as index. */
-uint32_t pkt_cnt[MAX_DEVICE] = {0};
+uint32_t pkt_cnt[MAX_DEVICE+1] = {0};   // sw_id is [1, 2, 3, 4, 5, 6], pkt_cnt[0] used for all. reset when write the record.
+uint32_t sw_cnt[MAX_DEVICE+1] = {0};    // count for sw in second, reset per second
 
 /* used for performance test per second. */
 uint32_t recv_cnt = 0, sec_cnt = 0, write_cnt = 0;
-uint64_t start_time = 0, end_time = 0;
-uint64_t start_time1 = 0, end_time1 = 0;
+double start_time[MAX_DEVICE+1] = {0}, end_time[MAX_DEVICE+1] = {0};
+double start_time1 = 0, end_time1 = 0;
 
 /* used for relative timestamp. */
-double relative_time = 0, delta_time = 0;        // write a record with a relative timestamp
-unsigned long long relative_start_time = 0;      // when first pkt comes in, timer runs
+double relative_time[MAX_DEVICE+1] = {0}, delta_time = 0;        // write a record with a relative timestamp
+double relative_start_time = 0;      // when first pkt comes in, timer runs
 bool first_pkt_in = true;                        // when first pkt comes in, turn 'false'
 
 /* used for INT item. */
 #define ITEM_SIZE 2048
 item_t int_data[ITEM_SIZE] = {0};
-float last_bd = 0;      // the last one bandwidth
+float last_bd[MAX_DEVICE+1] = {0};      // the last one bandwidth, last_bd[sw_id]
 
 static volatile bool force_quit;
 
@@ -362,6 +368,7 @@ static void process_int_pkt(struct rte_mbuf *m, unsigned portid) {
 #ifdef TEST_INT_HEADER
     uint16_t type = (pkt[pos++] << 8) + pkt[pos++];
     uint8_t ttl = pkt[pos++];
+//    printf("type:%d, ttl:%d", type, ttl);
     if (type != 0x0908 || ttl == 0x00) {
         return;
     }
@@ -370,20 +377,16 @@ static void process_int_pkt(struct rte_mbuf *m, unsigned portid) {
     /* first_int_pkt_in, init the 'relative_start_time' */
     if (first_pkt_in) {
         relative_start_time = rp_get_us();
-        start_time = relative_start_time;
+        for (int i=0; i < (MAX_DEVICE+1); i++) {
+            start_time[i] = relative_start_time;
+        }
         first_pkt_in = false;
-        printf(INT_STR);
-    }
-
-    /* calculate the relative time. */
-    end_time = rp_get_us();
-    relative_time = (end_time - relative_start_time) / ONE_SECOND_IN_MS;  // second
-    delta_time = end_time - start_time;
-
-    bool should_write = false;
-    if (delta_time > FIFTY_MS) { // 50ms, th2
-        should_write = true;
-        start_time = end_time;
+#ifndef PRINT_INT_FIELDS
+        printf(INT_FIELDS_STR, "@ok");
+#endif
+#ifdef PRINT_SW_CNT
+        printf(INT_PKT_CNT_STR, "@ok");
+#endif
     }
 
     /* if map_info doesn't contaion INT data. */
@@ -392,41 +395,79 @@ static void process_int_pkt(struct rte_mbuf *m, unsigned portid) {
         return;
     }
 
-    /*===================== PARSE STAGE =======================*/
-    uint32_t switch_id = 0x00;
-    if (map_info & 0x1) {
-        switch_id = (pkt[pos++] << 24) + (pkt[pos++] << 16) + (pkt[pos++] << 8) + pkt[pos++];
-
-    }
-
-    /* WARN: if 'switch_id' is set from 1, 2 ... otherwise, use 'ttl' as index. */
-    pkt_cnt[switch_id]++;      // used for counting the records
     recv_cnt++;                // used for how many packet processed in 1s, clear after per sec.
-
     int int_idx = recv_cnt % ITEM_SIZE;   //  current index
     int_data[int_idx].map_info = map_info;
 
-    if (map_info & (0x1 << 1)) {
-        int_data[int_idx].in_port = pkt[pos++];
-    }
+    /*===================== PARSE STAGE =======================*/
+    uint32_t switch_id = 0x00;
+    for (int i = 1; i <= ttl; i++) {    // ttl ranges from [1, 6]
+        if (map_info & 0x1) {
+            switch_id = (pkt[pos++] << 24) + (pkt[pos++] << 16) + (pkt[pos++] << 8) + pkt[pos++];
+            sw_cnt[switch_id]++;               // clear per second
+        }
 
-    if (map_info & (0x1 << 2)) {
-        int_data[int_idx].out_port = pkt[pos++];
-    }
+        pkt_cnt[switch_id]++;              // clear if printf
 
-    if (map_info & (0x1 << 3)) {
-        memcpy(&(int_data[int_idx].ingress_time), &pkt[pos], 8);
-        int_data[int_idx].ingress_time = ntohll(int_data[int_idx].ingress_time);
-        pos += 8;
-    }
+        /* calculate the relative time. */
+        end_time[switch_id] = rp_get_us();
+        relative_time[switch_id] = (end_time[switch_id] - relative_start_time) / ONE_SECOND_IN_MS;  // second
+        delta_time = end_time[switch_id] - start_time[switch_id];
 
-    if (map_info & (0x1 << 4)) {
-        int_data[int_idx].hop_latency = (pkt[pos++] << 8) + pkt[pos++];
-    }
+        bool should_write = false;
+        if (delta_time > FIFTY_MS) { // 50ms, th2
+            should_write = true;
+            start_time[switch_id] = end_time[switch_id];
+        }
 
-    if (map_info & (0x1 << 5)) {
-        memcpy(&(int_data[int_idx].bandwidth), &pkt[pos], 4);
-        pos += 4;
+        if (map_info & (0x1 << 1)) {
+            int_data[int_idx].in_port = pkt[pos++];
+        }
+
+        if (map_info & (0x1 << 2)) {
+            int_data[int_idx].out_port = pkt[pos++];
+        }
+
+        if (map_info & (0x1 << 3)) {
+            memcpy(&(int_data[int_idx].ingress_time), &pkt[pos], 8);
+            int_data[int_idx].ingress_time = ntohll(int_data[int_idx].ingress_time);
+            pos += 8;
+        }
+
+        if (map_info & (0x1 << 4)) {
+            int_data[int_idx].hop_latency = (pkt[pos++] << 8) + pkt[pos++];
+        }
+
+        if (map_info & (0x1 << 5)) {
+            memcpy(&(int_data[int_idx].bandwidth), &pkt[pos], 4);
+            pos += 4;
+        }
+
+#ifdef FILTER_PKTS
+        /*===================== FILTER STAGE =======================*/
+        /* we don't process no information updating packets. */
+        float delta_error = Minus(int_data[int_idx].bandwidth, last_bd[switch_id]) / Min(int_data[int_idx].bandwidth, last_bd[switch_id]);
+        last_bd[switch_id] = int_data[int_idx].bandwidth;
+
+        hash[switch_id] = simple_linear_hash(&int_data[int_idx]);
+
+        if ((delta_error > ERROR_THRESH) || (his_hash[switch_id] != hash[switch_id])
+            || should_write) {
+            start_time[switch_id] = end_time[switch_id];
+        } else {
+            continue;
+        }
+#endif
+
+        /* we also store cnt to show how many pkts we last stored as one record. */
+#ifndef PRINT_INT_FIELDS
+        printf(INT_FMT, int_data[int_idx].map_info, switch_id, int_data[int_idx].in_port,
+                int_data[int_idx].out_port, int_data[int_idx].hop_latency, int_data[int_idx].ingress_time,
+                int_data[int_idx].bandwidth, relative_time, pkt_cnt[switch_id]);
+#endif
+        his_hash[switch_id] = hash[switch_id];
+        pkt_cnt[switch_id] = 0;
+        write_cnt++;
     }
 
     /* output how many packets we can parse in a second. */
@@ -439,43 +480,29 @@ static void process_int_pkt(struct rte_mbuf *m, unsigned portid) {
     end_time1 = rp_get_us();
 
     if (end_time1 - start_time1 >= ONE_SECOND_IN_MS) {
+
+#ifndef PRINT_SECOND_PERFORMANCE
         /* second + recv_pkt/s + write/s */
         printf("%ds\t %d\t %d\n", sec_cnt, recv_cnt, write_cnt);
+#endif
+
+#ifdef PRINT_SW_CNT
+        printf("%ds\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\n", sec_cnt, recv_cnt, sw_cnt[0], sw_cnt[1], sw_cnt[2],
+                sw_cnt[3], sw_cnt[4], sw_cnt[5], sw_cnt[6]);
+#endif
+
         fflush(stdout);
         recv_cnt = 0;
         write_cnt = 0;
+        memset(sw_cnt, 0x00, sizeof(sw_cnt));
         start_time1 = end_time1;
     }
 
-    /* auto stop test. 'time_interval'=0 to disable. */
+    /* auto stop test. 'time_interval'=0 to disable to run. */
     if (!timer_interval || (sec_cnt > timer_interval)) {  // 15s in default, -R [interval] to adjust
         signal_handler(SIGINT);
     }
-
 #endif
-
-#ifdef FILTER_PKTS
-    /*===================== FILTER STAGE =======================*/
-    /* we don't process no information updating packets. */
-    float delta_error = Minus(int_data[int_idx].bandwidth, last_bd) / Min(int_data[int_idx].bandwidth, last_bd);
-    last_bd = int_data[int_idx].bandwidth;
-
-    hash[switch_id] = simple_linear_hash(&int_data[int_idx]);
-
-    if ((delta_error > ERROR_THRESH) || (his_hash[switch_id] != hash[switch_id])
-        || should_write) {
-        start_time = end_time;
-    } else {
-        return;
-    }
-#endif
-
-    /* we also store cnt to show how many pkts we last stored as one record. */
-    printf(INT_FMT, int_data[int_idx].map_info, switch_id, int_data[int_idx].in_port, int_data[int_idx].out_port,
-           int_data[int_idx].hop_latency, int_data[int_idx].ingress_time, int_data[int_idx].bandwidth, relative_time, pkt_cnt[switch_id]);
-    his_hash[switch_id] = hash[switch_id];
-    pkt_cnt[switch_id] = 0;
-    write_cnt++;
 }
 
 uint16_t pkt_cnt_v2 = 0;
@@ -566,17 +593,17 @@ item_t item = {
 
 #ifdef TEST_SECOND_PERFORMANCE
     if (recv_cnt == 1) {
-        start_time = rp_get_us();
+        start_time1 = rp_get_us();
         sec_cnt++;
     }
 
-    end_time = rp_get_us();
+    end_time1 = rp_get_us();
 
-    if (end_time - start_time >= 1000000) {
+    if (end_time1 - start_time1 >= 1000000) {
         printf("%d s processed %d packets/s\n", sec_cnt, recv_cnt);
         fflush(stdout);
         recv_cnt = 0;
-        start_time = end_time;
+        start_time1= end_time1;
     }
 #endif
 
